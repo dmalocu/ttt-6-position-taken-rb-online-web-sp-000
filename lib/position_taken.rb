6a75e148cfd368af5ab9121d2596ee1b19c3f507
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-06 18:58:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,6 @@
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     return FALSE
-  elseif board[index] == "X" || board[index] == "O"
-    return TRUE
   end
+  board[index] == "X" || board[index] == "O"
 end
